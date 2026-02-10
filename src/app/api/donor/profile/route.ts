cfc4f217c9a5435e56c14a36ecf3c424cc3bb04f
Ayush Singh <9751ayushsingh@gmail.com>
2026-02-10 17:11:06 +0530

    fix(api): force dynamic rendering for donor profile

--- src/app/api/donor/profile/route.ts
+++ src/app/api/donor/profile/route.ts
@@ -1,5 +1,6 @@
 
 import { NextResponse } from 'next/server';
+export const dynamic = 'force-dynamic';
 import dbConnect from '../../../../lib/dbConnect';
 import User from '../../../../models/User';
 import DonationAppointment from '../../../../models/DonationAppointment';
